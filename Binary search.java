b2da02dfd910926a25b39ad749d8e7e1cbb64b4f
subbaraoguggilam <subhashguggilam@gmail.com>
2026-02-02 23:03:22 +0530

    Create BinarySearch.java

---
+++ Binary search.java
@@ -0,0 +1,31 @@
+class BinarySearch {
+
+    static int binarySearch(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == key)
+                return mid;          // element found
+            else if (arr[mid] < key)
+                low = mid + 1;       // search right half
+            else
+                high = mid - 1;      // search left half
+        }
+        return -1; // element not found
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 10, 12, 14};
+        int key = 10;
+
+        int result = binarySearch(arr, key);
+
+        if (result == -1)
+            System.out.println("Element not found");
+        else
+            System.out.println("Element found at index: " + result);
+    }
+}
