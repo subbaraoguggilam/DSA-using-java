afd7babdd9716f3a021db41db12141d411afb05c
subbaraoguggilam <subhashguggilam@gmail.com>
2026-08-15 12:48:08 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 9458-java-stdin-and-stdout-ii-Hackerrank/9458-java-stdin-and-stdout-ii.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+        double d = scan.nextDouble();
+        String s = scan.nextLine();
+        scan.nextLine();
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
